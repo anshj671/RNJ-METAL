5f7f794a9e353121530467057799011baa8ee2f4
Neel-29 <bishnoin436@gmail.com>
2025-03-01 19:54:18 +0530

    footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,12 +1,57 @@
-import React from 'react'
-import './Footer.css'
+import { FaLinkedin, FaYoutube } from "react-icons/fa";
+import { FaX } from "react-icons/fa6";
+import "./Footer.css";
 
 const Footer = () => {
   return (
+    <footer>
+  <div className="footer-container">
     <div>
-      Footer sayslo!! ansh
+      <h3 className="footer-title">Our Solutions</h3>
+      <ul className="footer-links">
+        <li>Products / E-Shop</li>
+        <li>Assembly Technology Expert</li>
+        <li>Smart Factory Logistics</li>
+      </ul>
     </div>
-  )
-}
+    
+    <div>
+      <h3 className="footer-title">About RNJ</h3>
+      <ul className="footer-links">
+        <li>How we add value</li>
+        <li>About RNJ</li>
+        <li>Investor Relations</li>
+        <li>Jobs & Careers</li>
+      </ul>
+    </div>
 
-export default Footer
+    <div>
+      <h3 className="footer-title">Legal</h3>
+      <ul className="footer-links">
+        <li>Privacy Policy</li>
+        <li>Modify cookie preferences</li>
+        <li>General Terms and Conditions</li>
+        <li>Imprint</li>
+      </ul>
+    </div>
+
+    <div>
+      <h3 className="footer-title">Contact</h3>
+      <p className="footer-links">Contact us</p>
+    </div>
+  </div>
+
+  <div className="footer-bottom">
+    <p>Join our community and stay connected</p>
+    <div className="social-icons">
+      <FaLinkedin />
+      <FaX />
+      <FaYoutube />
+    </div>
+    <p className="copyright">Copyright © 2025 RNJ Metal. | All rights reserved.</p>
+  </div>
+  </footer>
+  );
+};
+
+export default Footer;
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import "./Home.css"
-import Navbar from "./Navbar.js"
+import React from 'react';
+import "./Home.css";
+import Navbar from "./Navbar.js";
+import Footer from './Footer.js';
+
 const Home = () => {
   return (
     <div className='home-container'>
         <Navbar />
-        <p className='Title-tag1'>Powering Industry</p>
-        <p className='Title-tag2'>with Precision</p>
-        <p className='Title-tag3'>Lubrication & Solutions</p>
+        <div className='hero-section'>
+            <p className='Title-tag1'>Powering Industry</p>
+            <p className='Title-tag2'>with Precision</p>
+            <p className='Title-tag3'>Lubrication & Solutions</p>
+            <h1 className='Title'>RNJ METAL</h1>
+        </div>
 
-        <h1 className='Title'>RNJ METAL</h1>
+        <Footer />
     </div>
   )
 }
 
-export default Home
+export default Home;
